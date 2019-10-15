6cbd7ad2616ed051075d6ccb263cc6b0a272ff69
fabiano maximiano <fabianomaximiano@hotmail.com>
2019-10-15 17:45:41 -0300

    single page

--- sobre.php
+++ sobre.php
@@ -12,9 +12,19 @@ $diretorio = get_template_directory_uri(); ?>
 					<p><?php the_author_description ();?></p>
 
 					<hr>
+						<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<figure>
+					<?php the_post_thumbnail( 'imagem-media' ); ?>
+					<figcaption><h2><span>SP</span><span>nome da cidade</span></h2></figcaption>
+				</figure>
+			</div>
+			<div class="col-md-8 col-sm-8 col-xs-8"">
+				<span><h3><?php the_title(); ?></h3></span>
+				<p><?php the_content(); //echo get_post_meta( get_the_ID(), 'descricao_evento', true ); ?></p>
+			</div>
 
-
-					<div class="row">
+<!-- 					<div class="row">
 						<div class="col-sm-6 col-md-4">
 							<div class="thumbnail">
 								<img src="images/thumbnails.png" alt="...">
@@ -44,7 +54,7 @@ $diretorio = get_template_directory_uri(); ?>
 									<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
 								</div>
 							</div>
-						</div>
+						</div> -->
 					</div>
 				</section>
 		</div>
